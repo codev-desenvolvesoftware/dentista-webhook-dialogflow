6df0790e620ef7d2c6d4d0bb3a6089d0ef373856
codev-desenvolvesoftware <codev.desenvolvesoftware@gmail.com>
2025-06-13 19:11:00 -0300

    refatorando para fluxo guiado no agendamento10

--- index.js
+++ index.js
@@ -585,6 +585,7 @@ app.post('/zapi-webhook', async (req, res) => {
     console.log("🤖 Resposta do bot:", reply);
     const intent = queryResult?.intent?.displayName;
     const parameters = queryResult?.parameters || {};
+    const outputContexts = response.queryResult.outputContexts || [];
 
     console.log("🔍 Contextos ativos:", queryResult.outputContexts);
     console.log("🧠 Intent recebida:", intent);
